fa2b8d2a0d85b0084a168e1f36d9b46bc868879d
dfpires@gmail.com <dfpires@gmail.com>
2022-02-16 15:16:10 -0300

    iniciando

---
+++ src/pages/index.tsx
@@ -0,0 +1,41 @@
+
+import { GetServerSideProps } from 'next'
+import { useEffect, useState } from 'react'
+import styles from '../styles/home.module.scss'
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  posts: Post[]
+}
+
+export default function Home({posts}: HomeProps) {
+
+  return (
+    <div>
+      <h1> 
+        Posts
+      </h1>
+      <ul>
+        {posts.map( post => (
+          <li key={post.id}> {post.title} </li>
+        ))}
+      </ul>
+    </div>
+    
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+
+  const response = await fetch('http://localhost:3333/posts')
+  const posts = await response.json()
+  return {
+    props: {
+      posts
+    }, 
+  }
+}
